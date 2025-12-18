fbb3733173f627cbf2d639c6e1132aeaddba55ac
Dobricean Ioan Dorian <50819975+dobri1408@users.noreply.github.com>
2025-12-18 11:53:48 +0200

    deploy api

--- api/src/config/index.ts
+++ api/src/config/index.ts
@@ -9,7 +9,9 @@ export const config = {
   firebase: {
     projectId: process.env.FIREBASE_PROJECT_ID || '',
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL || '',
-    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n') || '',
+    privateKey: (process.env.FIREBASE_PRIVATE_KEY || '')
+      .replace(/\\n/g, '\n')
+      .replace(/^["']|["']$/g, ''),
   },
 
   render: {
